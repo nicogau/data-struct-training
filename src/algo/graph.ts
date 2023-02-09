05d1f444adbb7af82fd410cd0f951c02e6ed67ce
nicolas G <nico@notmymail.com>
2023-02-09 15:58:31 +0100

    directed graph

--- src/algo/graph.ts
+++ src/algo/graph.ts
@@ -7,6 +7,8 @@
 // type NodesList =  typeof nodesList 
 // type AdjacencyList =  Record<NodesList[number], Array<NodesList[number]>>  
 // const nodesList = [ "a" , "b" , "c" , "d", "e", "f"] as const
+
+/* directed graph */
 const adjacencyList = {
   a: ["b","c"],
   b: ["d"],
@@ -25,66 +27,101 @@ const adjacencyList2 = {
   k: [],
 } 
 
+/* undirected graph */
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n'],
+] 
+
+const undirectedAdjacencyList = {
+  i: ['j', 'k'],
+  j: ['i'],
+  k: ['j', 'l', 'm'],
+  l: ['k'],
+  m: ['k'],
+  o: ['n'],
+  n: ['o'],
+
+
+}
+
 export const run = () => {
+  /* directed graph */
+  console.log('graph - depthFirst iterative traversal', depthFirst(adjacencyList, 'a'))
+
+  console.log('graph - depthFirst recursive traversal', depthFirstRec(adjacencyList, 'a'))
+
+  console.log('graph - breadthFirst traversal', breadthFirst(adjacencyList, 'a'))
+
+  console.log('graph - hasPathRec - depth first search recursive', hasPathRec(adjacencyList2, 'f', 'j'))
+
+  console.log('graph - hasPath - breadth first search', hasPathRec(adjacencyList2, 'f', 'j'))
+
+  /* undirected graph */
+
+}
+
+/**
+  * directed graph algorithms
+  *
+  */
 
 /* depth first traversal */
 //iterative
-  const depthFirst = (list: Record<string, string[]>, start: string) => {
-    let stack = [start]
-    let result: string[] = []
-    while (stack.length > 0) {
-      const current = stack.pop() as string
-      result.push(current)
-      if (list[current]?.length > 0) {
-        stack = [...stack, ...list[current]]
-      } 
-    }
-    return result
+const depthFirst = (list: Record<string, string[]>, start: string) => {
+  let stack = [start]
+  let result: string[] = []
+  while (stack.length > 0) {
+    const current = stack.pop() as string
+    result.push(current)
+    if (list[current]?.length > 0) {
+      stack = [...stack, ...list[current]]
+    } 
   }
-  console.log('graph - depthFirst iterative traversal', depthFirst(adjacencyList, 'a'))
+  return result
+}
 
-  /* depth first traversal */
-  // recursive
-  const depthFirstRec = (list: Record<string, string[]>, start: string): string[] => {
-    let res: string[] = [] 
+// recursive
+const depthFirstRec = (list: Record<string, string[]>, start: string): string[] => {
+  let res: string[] = [] 
 
-    if (list[start].length > 0 ){ 
-      for (let el of list[start]) {
-        res = [...res, ...depthFirstRec(list, el)]
-        console.log(el)
-      }
+  if (list[start].length > 0 ){ 
+    for (let el of list[start]) {
+      res = [...res, ...depthFirstRec(list, el)]
+      console.log(el)
     }
-     return [start, ...res] 
   }
-  console.log('graph - depthFirst recursive traversal', depthFirstRec(adjacencyList, 'a'))
+   return [start, ...res] 
+}
 
-  /* breadth first traversal */
-  const breadthFirst = (list: Record<string, string[]>, start: string): string[] => {
-    let queue = [start]
-    let res: string[] = []
+/* breadth first traversal */
+const breadthFirst = (list: Record<string, string[]>, start: string): string[] => {
+  let queue = [start]
+  let res: string[] = []
 
-    while (queue.length > 0) {
-      const current = queue.pop()
-      if (!current) return res
+  while (queue.length > 0) {
+    const current = queue.pop()
+    if (!current) return res
 
-      res.push(current)
-      for (let el of list[current]) {
-        queue.unshift(el)
-      }
+    res.push(current)
+    for (let el of list[current]) {
+      queue.unshift(el)
     }
-    return res
   }
-  console.log('graph - breadthFirst traversal', breadthFirst(adjacencyList, 'a'))
+  return res
+}
 
-  /* finding if a path exist from the root node to a defined vertex */
-  /* depth first search recursive */
-  const hasPathRec = (list: Record<string, string[]>, root: string, dest: string ): boolean => {
-    for (let el of list[root]) {
-      if (hasPathRec(list, el, dest)) return true
-    }
-    return root === dest
+/* finding if a path exist from the root node to a defined vertex */
+/* depth first search recursive */
+const hasPathRec = (list: Record<string, string[]>, root: string, dest: string ): boolean => {
+  for (let el of list[root]) {
+    if (hasPathRec(list, el, dest)) return true
   }
-  console.log('graph - hasPathRec - depth first search recursive', hasPathRec(adjacencyList2, 'f', 'j'))
+  return root === dest
+}
 
 /* breadth first search iterative */
   const hasPath = (list: Record<string, string[]>, root: string, dest: string ): boolean => {
@@ -99,5 +136,8 @@ export const run = () => {
     }
     return false
   }
-  console.log('graph - hasPath - breadth first search', hasPathRec(adjacencyList2, 'f', 'f'))
-}
+
+/**
+  * undirected graph algorithms
+  *
+  */
